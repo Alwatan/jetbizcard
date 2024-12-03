bbe118358f856f5ea2d9af16e7ec53151a3dcbbe
Alwatan <pugudean@gmail.com>
2024-12-03 13:29:28 +0300

    added user card

--- app/src/main/java/tz/co/dbs/jetbizcard/MainActivity.kt
+++ app/src/main/java/tz/co/dbs/jetbizcard/MainActivity.kt
@@ -4,14 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import tz.co.dbs.jetbizcard.ui.theme.JetBizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +47,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard(modifier: Modifier = Modifier) {
-    Surface {  }
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Card(
+            modifier = Modifier.width(200.dp).height(390.dp).padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(24.dp)),
+            colors = CardDefaults.cardColors(containerColor = Color.White),
+            elevation = CardDefaults.cardElevation(4.dp)) {
+            Surface(
+                modifier = Modifier.size(150.dp).padding(4.dp),
+                shape = CircleShape,
+                color = MaterialTheme.colorScheme.surface.copy(alpha = 0.5f),
+                border = BorderStroke(0.5.dp, Color.LightGray),
+            ) {
+                Image(
+                    painter = painterResource(
+                        id = R.drawable.user),
+                    contentDescription = "User",
+                )
+            }
+        }
+    }
 }
 
 @Composable
