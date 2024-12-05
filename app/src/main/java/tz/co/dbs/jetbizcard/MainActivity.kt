f989e17a1c04633d6ae32309abc047db6810ae21
Alwatan <pugudean@gmail.com>
2024-12-05 09:47:29 +0300

    added portfolio listt

--- app/src/main/java/tz/co/dbs/jetbizcard/MainActivity.kt
+++ app/src/main/java/tz/co/dbs/jetbizcard/MainActivity.kt
@@ -6,15 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -27,10 +32,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import tz.co.dbs.jetbizcard.ui.theme.JetBizCardTheme
@@ -53,29 +63,39 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard(modifier: Modifier = Modifier) {
+
+    val previewState = remember {
+        mutableStateOf(true)
+    }
+
     Surface(
         modifier = modifier.fillMaxSize()
     ) {
         Card(
-            modifier = modifier.width(100.dp).height(390.dp).padding(12.dp),
+            modifier = modifier.padding(12.dp),
             shape = RoundedCornerShape(corner = CornerSize(24.dp)),
             colors = CardDefaults.cardColors(containerColor = Color.White),
             elevation = CardDefaults.cardElevation(4.dp)) {
             Column(
-                modifier = modifier.height(300.dp).fillMaxWidth(),
+                modifier = modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Profile()
                 HorizontalDivider()
                 UserInfo()
-                PortfolioBtn()
+                PortfolioBtn(previewState)
+                if(previewState.value){
+                    Content()
+                } else {
+                    Box{ }
+                }
             }
         }
     }
 }
 
-@Preview
+//@Preview
 @Composable
 fun Content(modifier: Modifier = Modifier) {
     Box(
@@ -92,15 +112,50 @@ fun Content(modifier: Modifier = Modifier) {
 }
 
 @Composable
-private fun PortfolioBtn() {
-    Button(onClick = {}) {
+private fun PortfolioBtn(previewState: MutableState<Boolean>) {
+    Button(onClick = {
+        previewState.value = !previewState.value
+    }) {
         Text(text = "Portfolio")
     }
 }
 
 @Composable
-private fun PortfolioList(data: List<String>) {
-
+private fun PortfolioList(data: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn {
+        items(data) { item ->
+            Card(
+                modifier = modifier.padding(12.dp).fillMaxWidth(),
+                shape = RectangleShape,
+                elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+            ) {
+                Row(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .height(96.dp)
+                        .background(MaterialTheme.colorScheme.surface.copy(alpha = 0.5f))
+                        .padding(16.dp)
+                ) {
+                    Profile(modifier = modifier.size(64.dp))
+                    Spacer(modifier = modifier.padding(6.dp))
+                    Column(
+                        modifier = modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = item,
+                            style = MaterialTheme.typography.bodyLarge,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = "A great Project Indeed",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Composable
@@ -126,9 +181,9 @@ private fun UserInfo(modifier: Modifier = Modifier) {
 }
 
 @Composable
-private fun Profile() {
+private fun Profile(modifier: Modifier = Modifier) {
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .size(150.dp)
             .padding(4.dp),
         shape = CircleShape,
@@ -140,7 +195,7 @@ private fun Profile() {
                 id = R.drawable.user
             ),
             contentDescription = "User",
-            modifier = Modifier.size(135.dp)
+            modifier = modifier.size(135.dp)
         )
     }
 }
